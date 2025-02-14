12c1430a9412804e76443753df89c8fc81988207
Hemanth939281 <hkh777650@gmail.com>
2025-02-15 01:06:45 +0530

    corrected createquiz import in router

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/Layout";
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
-import CreateQuiz from "../pages/createQuiz";
+import CreateQuiz from "../pages/CreateQuiz";
 import EditQuiz from "../pages/EditQuiz";
 import NotFound from "../pages/NotFound";
 
