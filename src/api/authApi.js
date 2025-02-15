b305c3007bc1fab14221921cc95a212d29f098f1
Hemanth939281 <hkh777650@gmail.com>
2025-02-15 10:42:54 +0530

    changed endpoint url of login api

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -2,7 +2,7 @@ const API_URL = import.meta.env.VITE_API_URL || "https://quizo-0yib.onrender.com
 
 export const loginUser = async (username, password) => {
   try {
-    const response = await fetch(`${API_URL}/auth/login`, {
+    const response = await fetch(`${API_URL}/api/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
